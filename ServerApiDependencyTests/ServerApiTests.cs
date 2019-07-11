ee44bc91cd40921dc01e89550a1126c48635e90c
Steven Chen <steven.chen19@TI03NB69>
2019-07-11 18:28:21 +0800

    The Server Api Test Pass

--- ServerApiDependencyTests/ServerApiTests.cs
+++ ServerApiDependencyTests/ServerApiTests.cs
@@ -1,18 +1,75 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerApiDependency;
+using ServerApiDependency.Enums;
+using ServerApiDependency.Utility.CustomException;
+using System;
+using System.Net;
+using NSubstitute.Routing.Handlers;
+using ServerApiDependency.Enum;
 
-namespace ServerApiDependency.Tests
+namespace ServerApiDependencyTests
 {
+    public class FakeRecorder : IRecorder
+    {
+        public void RecordError(string message)
+        {
+            //DO NOTHING
+        }
+    }
+
+    public class FakeServerApi : ServerApi
+    {
+        public FakeServerApi()
+        {
+            Recorder = new FakeRecorder();
+        }
+
+        public static int SaveFailRequestToDbCalledCount { get; set; }
+
+        internal override int PostToThirdParty(ApiType apiType, string apiPage)
+        {
+            throw new WebException();
+        }
+
+        internal override void SaveFailRequestToDb(ApiType ApiType, string ApiPage)
+        {
+            SaveFailRequestToDbCalledCount++;
+        }
+    }
+
+    public class FakeServerApi1 : ServerApi
+    {
+        private readonly int _returnNumber;
+
+        public FakeServerApi1(int number)
+        {
+            _returnNumber = number;
+            Recorder = new FakeRecorder();
+        }
+
+        internal override int PostToThirdParty(ApiType apiType, string apiPage)
+        {
+            return _returnNumber;
+        }
+    }
+
     [TestClass()]
     public class ServerApiTests
     {
+        private ServerApi _api;
+
         /// <summary>
-        /// LV 1, verify api correct response
+        /// LV 3, verify specific method be called
         /// </summary>
         [TestMethod()]
-        public void post_cancelGame_third_party_success_test()
+        public void post_cancelGame_third_party_exception_test()
         {
-            // Assert success
-            Assert.Fail();
+            // Assert SaveFailRequestToDb() be called once
+            _api = new FakeServerApi();
+            Action action = () => _api.CancelGame();
+            action.Should().Throw<WebException>();
+            Assert.AreEqual(1, FakeServerApi.SaveFailRequestToDbCalledCount);
         }
 
         /// <summary>
@@ -22,17 +79,19 @@ namespace ServerApiDependency.Tests
         public void post_cancelGame_third_party_fail_test()
         {
             // Assert PostToThirdParty() return not correct should throw AuthFailException
-            Assert.Fail();
+            _api = new FakeServerApi1(99);
+            Assert.ThrowsException<AuthFailException>(() => _api.CancelGame());
         }
 
         /// <summary>
-        /// LV 3, verify specific method be called
+        /// LV 1, verify api correct response
         /// </summary>
         [TestMethod()]
-        public void post_cancelGame_third_party_exception_test()
+        public void post_cancelGame_third_party_success_test()
         {
-            // Assert SaveFailRequestToDb() be called once
-            Assert.Fail();
+            // Assert success
+            _api = new FakeServerApi1(0);
+            Assert.AreEqual(ServerResponse.Correct, _api.CancelGame());
         }
     }
 }
